920f025f18b1c81547b5330452f2a312ad81d53c
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-08-14 18:11:34 -0600

    llenar info de los compradores...

---
+++ Ejercicio1/Ejercicio1/Comprador.java
@@ -0,0 +1,20 @@
+package Ejercicio1; 
+import java.util.*; 
+public class Comprador { 
+    //declaracion de los atibutos de la clase Comprador.java 
+    private String nombre; 
+    private String email; 
+    private int Nboletos; 
+    private int PMaximo; 
+    private Random AleatorioAsignado; 
+
+    public Comprador() { 
+        nombre =""; 
+        email=""; 
+        Nboletos=0; 
+        PMaximo=0; 
+        AleatorioAsignado = new Random(); 
+    } 
+
+    public Comprador()
+}
