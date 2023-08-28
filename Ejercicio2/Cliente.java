fc65cb4ecae1db8b4bbf58e7d43943e72ba56f03
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-08-28 00:12:49 -0600

    Creacion de la clase cliente

---
+++ Ejercicio2/Cliente.java
@@ -0,0 +1,39 @@
+package Ejercicio2;
+/**
+ * Esta es la clase donde se ira organizando o guardando las distintas personas que se van hospedando en el hotel. 
+ * yo considero que mejor aqui va el arreglo de lista de espera... 
+ */
+public class Cliente{ 
+    private String Nombre; 
+    private int Noches; 
+    private int TipoCliente; 
+
+    public Cliente() { 
+    } 
+    public Cliente(String Nombre, int Noches, int TipoCliente) { 
+        this.Nombre = Nombre; 
+        this.Noches = Noches; 
+        this.TipoCliente =TipoCliente; 
+    } 
+    /**
+     * Los metodos get y set
+     */ 
+    public String getNombre() { 
+        return this.Nombre; 
+    } 
+    public void setNombre(String Nombre) { 
+        this.Nombre =Nombre; 
+    } 
+    public int getNoches() { 
+        return this.Noches; 
+    } 
+    public void setNoches(int Noches) { 
+        this.Noches=Noches; 
+    } 
+    public int getTipoCliente() { 
+        return this.TipoCliente; 
+    } 
+    public void setTipoCliente(int TipoCliente) { 
+        this.TipoCliente = TipoCliente; 
+    } 
+}
