d073d4323e34374adb6d5ef63bb962c2b75ed564
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-11-05 22:08:48 -0600

    Update Principal.java

--- Laboratorio4PolimorfismoViaInterfaces/Principal.java
+++ Laboratorio4PolimorfismoViaInterfaces/Principal.java
@@ -2,6 +2,8 @@ package Laboratorio4PolimorfismoViaInterfaces;
 
 public class Principal {
     public static void main (String[]args) { 
-        
+        //adad 
+        IReservaUsuario Usuario = new Usuario(); 
+        System.out.println("Ingrese su nombre...");  
     }
 }
